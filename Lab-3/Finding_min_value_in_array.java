314af3861143256b251ab2dcc6b26a7db5da02f0
ankit <ac2000choudhary@gmail.com>
2021-05-15 14:29:58 +0530

    Finding min value in array

---
+++ Lab-3/Finding_min_value_in_array.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class Finding_min_value_in_array
+{
+    public static void min(int a[])
+    {
+        int min = a[0];
+
+        for (int i = 0; i <a.length; i++)
+        {
+            if(min > a[i])
+            
+            {
+                min = a[i];
+            }
+        }
+        System.out.println("Minimum element in Array is : " + min);
+    }
+    
+    public static void main(String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int size = sc.nextInt();
+
+        // Declaring array
+        int a[] = new int[size];
+
+        System.out.println("Enter the value in array: ");
+
+        for (int i = 0; i <size; i++)
+        {
+            System.out.print(">> ");
+            a[i] = sc.nextInt();
+        }
+        min(a);
+
+        sc.close();
+    }
+}
