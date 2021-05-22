d3988479b5259b0add1e12eb2258e88e2e39ac30
ankit <ac2000choudhary@gmail.com>
2021-05-22 14:55:59 +0530

    Adding Complex Number

--- LAB-4/ParameterizedConstructor.java
+++ LAB-4/ParameterizedConstructor.java
@@ -3,6 +3,7 @@ class ParameterizedConstructor
 {
     int id;
     String name;
+    int age;
 
     ParameterizedConstructor(int i, String n)
     {
@@ -10,14 +11,21 @@ class ParameterizedConstructor
         name = n;
     }
 
+    ParameterizedConstructor(int i, String n, int j)
+    {
+        id = i;
+        name = n;
+        age = j;
+    }
+
     void display(){
-                System.out.println("Id: " + id + "\nName: " + name + "\n************************************" );
+                System.out.println("Id: " + id + "\nName: " + name + "\nAge: "+ age + "\n************************************" );
     }
 
     public static void main(String[] args)
     {
         ParameterizedConstructor S1 = new ParameterizedConstructor(111, "Raj");
-        ParameterizedConstructor S2 = new ParameterizedConstructor(123, "Harry");
+        ParameterizedConstructor S2 = new ParameterizedConstructor(123, "Harry", 25);
 
         S1.display();
         S2.display();
