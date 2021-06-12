b2f35171780b18f0e0cf79a9943a1389f0c8d143
ankit <ac2000choudhary@gmail.com>
2021-06-12 14:27:55 +0530

    overridingVehicle run function

---
+++ LAB-6/overriding.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+class Vehicle {
+    void run() {
+        System.out.println("Vehicle is running");
+    }
+}
+
+class overriding extends Vehicle {
+    void run() {
+        System.out.println("Bike is running");
+    }
+    public static void main(String[] args){
+        overriding obj = new overriding();
+        obj.run();
+    }
+}
