caab6ab0785abc4c899f8aba37368914fe651831
ankit <ac2000choudhary@gmail.com>
2021-06-12 14:43:36 +0530

    overriding

--- LAB-6/overriding.java
+++ LAB-6/overriding.java
@@ -5,12 +5,15 @@ class Vehicle {
     void run() {
         System.out.println("Vehicle is running");
     }
+   
 }
 
 class overriding extends Vehicle {
     void run() {
+        super.run();
         System.out.println("Bike is running");
     }
+    
     public static void main(String[] args){
         overriding obj = new overriding();
         obj.run();
