a9359084a1815330a87b9c255df31232b495cf04
ankit <ac2000choudhary@gmail.com>
2021-06-12 13:29:02 +0530

    overloading done

--- LAB-6/Adder.java
+++ LAB-6/Adder.java
@@ -9,6 +9,9 @@ class Test {
     static int add(int a, int b, int c) {
         return a + b + c;
     }
+    static double add(double a, double b, double c) {
+        return a + b + c;
+    }
 }
 
 class Adder {
@@ -16,5 +19,6 @@ class Adder {
     {
         System.out.println("Addition of 20 + 30 is:- " + Test.add(20, 30));
         System.out.println("Addition of 20 + 30 + 10 is:- " + Test.add(20, 30,10));
+        System.out.println("Addition of 20 + 30 + 10 is:- " + Test.add(20.0, 30.0,10.0));
     }
 }
