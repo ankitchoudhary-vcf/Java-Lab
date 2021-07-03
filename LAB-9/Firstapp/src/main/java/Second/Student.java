3dbede1f1ff03933a41001974801759a18eb11d8
ankit <ac2000choudhary@gmail.com>
2021-07-03 13:50:32 +0530

    Encapsulation

---
+++ LAB-9/Firstapp/src/main/java/Second/Student.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Second;
+
+/**
+ *
+ * @author Ankit
+ */
+public class Student {
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ LAB-9/Firstapp/src/main/java/Second/Test.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Second;
+
+/**
+ *
+ * @author Ankit
+ */
+public class Test {
+    public static void main(String[] args){
+        Student s = new Student();
+        s.setName("Harry");
+        System.out.println("Student Name is: "+ s.getName());
+    }
+}
