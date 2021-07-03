57e53369ff723ce37c7c84ce30f00450e0482d1d
ankit <ac2000choudhary@gmail.com>
2021-07-03 14:14:35 +0530

    TestAbstraction

---
+++ LAB-9/TestAbstraction.java
@@ -0,0 +1,23 @@
+
+abstract class Shape {
+    abstract void draw();
+}
+
+class Rectangle extends Shape {
+    void draw(){
+        System.out.println("Drawing Rectangle");
+    }
+}
+
+class Circle extends Shape {
+    void draw(){
+        System.out.println("Drawing Circle");
+    }
+}
+
+class TestAbstraction {
+    public static void main(String[] args) {
+        Shape s = new Circle();
+        s.draw();
+    }
+}
