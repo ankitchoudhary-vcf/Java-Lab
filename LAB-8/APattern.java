1f1f2ce9a5a4ebec9f42e0ecd89a1890dcbde429
ankit <ac2000choudhary@gmail.com>
2021-07-04 07:46:47 +0530

    A Pattnern added

--- LAB-8/APattern.java
+++ LAB-8/APattern.java
@@ -10,14 +10,25 @@ public class APattern
         {  
 
         
-            for (j = 0; j<= n / 2; j++)   
-            {  
-                if ((j == 0 || j == n / 2) && i != 0 ||  i == 0  && j != n / 2 || i == n / 2)   
-                    System.out.print("*");  
-                else  
-                    System.out.print(" ");  
-            }  
-            System.out.println();  
+            // for (j = 0; j<= n / 2; j++)   
+            // {  
+            //     if ((j == 0 || j == n / 2) && i != 0 ||  i == 0  && j != n / 2 || i == n / 2)   
+            //         System.out.print("*");  
+            //     else  
+            //         System.out.print(" ");  
+            // }  
+            // System.out.println();  
+
+            for(j=0; j<n; j++){
+                if((j==0 || j==n-1 || i==0 || i==n/2))
+                {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
         }  
     }  
 }  
