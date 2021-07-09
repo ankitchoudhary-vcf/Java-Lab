e89c599970ba0f198036a27ea66c0f1919976832
ankit <ac2000choudhary@gmail.com>
2021-07-09 13:33:55 +0530

    interface example-2

---
+++ LAB-11/public class interface_example.java
@@ -0,0 +1,19 @@
+
+
+interface Printable {
+    void Print();
+}
+
+
+class interface_example implements Printable{
+    
+    public void Print() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        interface_example example = new interface_example();
+        example.Print();
+    }
+    
+}
