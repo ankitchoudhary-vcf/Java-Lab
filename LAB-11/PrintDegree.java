f7cfc71281814c20fa325b64bf3643acd271638a
ankit <ac2000choudhary@gmail.com>
2021-07-09 14:51:00 +0530

    Printing Degree

---
+++ LAB-11/PrintDegree.java
@@ -0,0 +1,29 @@
+
+class Degree {
+    void getDegree() {
+        System.out.println("I got a degree");
+    }
+}
+
+class Undergraduate extends Degree {
+    void getDegree() {
+        System.out.println("I am an Undergraduate");
+    }
+}
+
+class Postgraduate extends Degree {
+    void getDegree() {
+        System.out.println("I am a Postgraduate");
+    }
+}
+
+public class PrintDegree {
+    public static void main(String[] args) {
+        Degree d = new Degree();
+        Undergraduate u = new Undergraduate();
+        Postgraduate p = new Postgraduate();
+        d.getDegree();
+        u.getDegree();
+        p.getDegree();
+    }
+}
